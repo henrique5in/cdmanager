7443da7df5fab089d46daf139bd51ca636fee2a1
Henrique Rodrigues da Silva <henrique.5in@gmail.com>
2020-09-21 11:47:18 -0300

    Create create_cd.sql

---
+++ sql/create_cd.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `cd` (
+	`id_cd` INT(11) NOT NULL AUTO_INCREMENT,
+	`nome` VARCHAR(45) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`artista` VARCHAR(45) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`ano_lancamento` VARCHAR(45) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`genero` VARCHAR(45) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`duracao` VARCHAR(45) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id_cd`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
